45531b80d43aa6f8c55bfa8dfb5a9e7ebd84428b
lequyet2001 <lequyet840500@gmail.com>
2020-11-03 14:26:02 +0700

    update

--- js/main.js
+++ js/main.js
@@ -39,6 +39,44 @@ $('.responsive').slick({
 
 
 
+$('.slider-banner').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed: 1500,
+    arrow:true,
+    responsive: [
+      {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      }
+    ]
+});
+
+
+
+
 let timneOut;
 let btn_scroll = document.getElementById("btn-btt");
 scrollToTop = () =>{
